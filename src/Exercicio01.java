ec6c954a85a7ef3dbc6ca5f19ecce32e3b6f6ee0
iddsp <progdanielsouza@proton.me>
2026-03-19 15:16:37 -0300

    Criando projeto

---
+++ src/Exercicio01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x ;
+		int y ;
+		int soma;
+		
+		
+		System.out.println("Digite o primeiro Valor:");
+		x = sc.nextInt();
+		
+		System.out.println("Digite o segundo Valor:");
+		y = sc.nextInt();
+			
+		soma = x + y;
+		
+		sc.close();
+		System.out.println("Soma = " + soma);
+		
+	}
+
+
+
+}
---
+++ src/Exercicio02.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+/*
+	Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais conforme exemplos.
+	Fórmula da área: area = π . raio2
+	Considere o valor de π = 3.14159 
+
+*/
+
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double raio ;
+		double pi = 3.14159;
+		double area;
+		
+		
+		System.out.println("Digite o Raio:");
+		raio = sc.nextDouble();
+
+		area = (pi * Math.pow(raio, 2));
+		
+		sc.close();
+		System.out.printf("A area é = %.4f%n ", area);
+
+	}
+
+}
