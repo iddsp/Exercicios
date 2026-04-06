f2e3c5b0ff435d68d29cd18f86ced834acc22a60
iddsp <progdanielsouza@proton.me>
2026-04-06 13:04:58 -0300

    Parte2 Exercicio 07 e 08 finalizados

--- src/Parte2/Exercicio07.java
+++ src/Parte2/Exercicio07.java
@@ -1,10 +1,42 @@
 package Parte2;
+/*
+ Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, 
+ ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0). Se o ponto estiver na origem, escreva a mensagem “Origem”. Se o ponto estiver sobre um dos eixos escreva
+ “Eixo X” ou “Eixo Y”, conforme for a situação.
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicio07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Digite o Valo de X:");
+		double x = sc.nextDouble();
+		System.out.println("Digite o Valor de Y:");
+		double y = sc.nextDouble();
+		
+		if(x == y && y == 0) {
+			System.out.println("Origem");
+		}else if (x == 0.0) {
+			System.out.println("Eixo Y");
+		}else if (y == 0.0) {
+			System.out.println("Eixo X");
+		}else if(x > 0 && y > 0) {
+			System.out.println("Q1");
+		}else if(x < 0 && y > 0) {
+			System.out.println("Q2");
+		}else if(x < 0 && y < 0) {
+			System.out.println("Q3");
+		}else if(x > 0 && y < 0) {
+			System.out.println("Q4");
+		}
+		
+		
+		sc.close();
 	}
 
 }
---
+++ src/Parte2/Exercicio08.java
@@ -0,0 +1,45 @@
+package Parte2;
+/*
+ Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
+que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
+qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
+Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e
+mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
+
+ * */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio08 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite o Valor do Salario:");
+		double salario = sc.nextDouble();
+		double imposto = 0;
+		
+		if(salario <= 2000) {
+			
+			System.out.println("Insento!");
+			
+		}else if (salario < 3000 ) {
+			
+			imposto = (salario - 2000) * 0.08;
+				
+		}else if(salario > 3000 && salario <= 4500) {
+			
+			imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+			
+			
+		}else if (salario > 4500){
+			
+			imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+		}
+		System.out.printf("R$: %.2f%n ", imposto);
+		sc.close();
+	}
+
+}
